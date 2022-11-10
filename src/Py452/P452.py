fa32b58f7c451a4fe391d921330d16f230a808f5
eeveetza <ivica.stevanovic@bakom.admin.ch>
2022-11-10 19:29:38 +0100

    patch to resolve issue #1
    Corrected the related bugs in subroutines path_fraction  and longest_cont_dist (see eeveetza/p1812#9)

--- src/Py452/P452.py
+++ src/Py452/P452.py
@@ -958,6 +958,7 @@ def  path_fraction(d, zone, zone_r):
      Rev   Date        Author                          Description
      -------------------------------------------------------------------------------
      v0    17MAR22     Ivica Stevanovic, OFCOM         First implementation in python
+     v1    11NOV22     Ivica Stevanovic, OFCOM         Corrected a bug in the second if clause (suggested by Martin-Pierre Lussier @mplussier)
     """
     dm = 0
 
@@ -972,7 +973,7 @@ def  path_fraction(d, zone, zone_r):
         
         
         if ( d[start[i]] > 0 ) :
-            delta = delta + ( d[stop[i]]-d[stop[i]-1] )/2.0
+            delta = delta + ( d[start[i]]-d[start[i]-1] )/2.0
         
         dm = dm + d[stop[i]]-d[start[i]] + delta
 
@@ -1003,7 +1004,8 @@ def longest_cont_dist(d, zone, zone_r):
     Rev   Date        Author                          Description
     -------------------------------------------------------------------------------
     v0    18MAR22     Ivica Stevanovic, OFCOM         Initial version
-         
+    v1    10NOV22     Ivica Stevanovic, OFCOM         Corrected a bug (as suggested by Martin-Pierre Lussier @mplussier)
+
     """     
     dm = 0
 
@@ -1021,7 +1023,7 @@ def longest_cont_dist(d, zone, zone_r):
         
         
         if (d[start[i]]>0):
-            delta = delta + ( d[stop[i]]-d[stop[i]-1] )/2.0
+            delta = delta + ( d[start[i]]-d[start[i]-1] )/2.0
         
         
         dm = max(d[stop[i]]-d[start[i]] + delta, dm)
